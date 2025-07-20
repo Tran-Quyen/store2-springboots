8d133ce93f39f68e7b00f372e7e7f83bd6b9dda0
Tran-Quyen <quyen10a8.sao@gmail.com>
2025-07-20 23:47:44 +0700

    Updated.

---
+++ src/main/java/com/tranquyen/store2/model/User.java
@@ -0,0 +1,17 @@
+package com.tranquyen.store2.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "dm_user")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+}
